91d11ea597453b54b8f35ec0b3fc452b63a7cfd7
AhMing <keeshengming@outlook.com>
2023-09-10 22:39:04 +0800

    Update version text to "v2.1.0"

--- src/main/java/com/ahming/ui/labels/VersionLabel.java
+++ src/main/java/com/ahming/ui/labels/VersionLabel.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class VersionLabel extends JLabel {
 
     public VersionLabel() {
-        this.setText("v2.0.0");
+        this.setText("v2.1.0");
         this.setHorizontalAlignment(JLabel.CENTER);
     }
 
